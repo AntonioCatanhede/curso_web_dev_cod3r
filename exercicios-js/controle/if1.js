33c284390b0ce832ba364e8217343f3bfa852427
Antonio Cantanhede <adcant@gmail.com>
2019-01-03 16:51:59 -0200

    adicionando segundo modulo do curso

---
+++ exercicios-js/controle/if1.js
@@ -0,0 +1,16 @@
+function sobBoaNoticia(nota){
+    if(nota >= 7){
+        console.log('Aprovado com ' + nota)
+    }
+}
+
+sobBoaNoticia (8.1)
+sobBoaNoticia (6.1)
+
+function seForVerdade (valor){
+    if(valor){
+        console.log('É verdade ... ' + valor)
+    }
+}
+
+seForVerdade(!!1)
